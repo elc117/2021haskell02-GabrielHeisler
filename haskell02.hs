cfaf3ced99234156f3c4f33ef6047f0198c83112
Gabriel Heisler <gvheisler@inf.ufsm.br>
2021-05-30 16:38:52 -0300

    changes on haskell02

--- haskell02.hs
+++ haskell02.hs
@@ -1,2 +1,20 @@
 -- Prática 02 de Haskell
 -- Nome: Gabriel Vinícius Heisler
+
+isFebre :: Float -> Bool
+isFebre x = x > 37.8
+
+comFebre :: [Float] -> [Float]
+comFebre list = filter isFebre list
+
+comFebre' :: [Float] -> [Float]
+comFebre' list = filter (\x -> x>37.8) list
+
+itemize :: [String] -> [String]
+itemize str = map (\str -> "<li>" ++ str ++ "</li>") str
+
+areaMaior :: Float -> Float -> Bool
+areaMaior num r = r^2 * pi > num
+
+bigCircles :: Float -> [Int] -> [Float]
+bigCircles num lista = filter areaMaior num lista
